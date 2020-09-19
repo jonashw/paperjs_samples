0d7d5d971a03567b37a3f8a255bb5c9820f6bf6c
Jon Wilson <jonwilson@powerusers-mbp.lan>
2020-09-18 23:00:50 -0700

    more interesting emoji  animations

--- js/emoji_sample.js
+++ js/emoji_sample.js
@@ -36,6 +36,37 @@ texts =
         content: e
     })));
 
+var easeFns = [
+    e => 2 * Math.sin(e.time%1),
+    e => 5 * Math.sin(e.time),
+    e => 2 * easeInOutBounce((e.time/12)%1),
+];
+
 function onFrame(e){
-    texts.forEach(function(t){ t.rotate(1) });
+    
+    texts.forEach((t,i) => {
+        let theta = easeFns[i % easeFns.length](e);
+        t.rotate(theta);
+    });
+}
+
+function easeInOutBounce(x){
+    return x < 0.5
+        ? (1 - easeOutBounce(1 - 2 * x)) / 2
+        : (1 + easeOutBounce(2 * x - 1)) / 2;
+}
+
+function easeOutBounce(x) {
+    const n1 = 7.5625;
+    const d1 = 2.75;
+    
+    if (x < 1 / d1) {
+        return n1 * x * x;
+    } else if (x < 2 / d1) {
+        return n1 * (x -= 1.5 / d1) * x + 0.75;
+    } else if (x < 2.5 / d1) {
+        return n1 * (x -= 2.25 / d1) * x + 0.9375;
+    } else {
+        return n1 * (x -= 2.625 / d1) * x + 0.984375;
+    }
 }
